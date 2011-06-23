c424786f5af17c8b96dd3621916da68271e52c5a
Jon Nordby <jononor@gmail.com>
2011-06-23 20:55:40 +0200

    Fix up license headers
    Make it clear that the files are part of GEGL-GTK,
    and that the library code is LGPLv3+

--- gegl-gtk/gegl-gtk-view.h
+++ gegl-gtk/gegl-gtk-view.h
@@ -1,19 +1,19 @@
-/* This file is part of GEGL editor -- a gtk frontend for GEGL
+/* This file is part of GEGL-GTK
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * GEGL-GTK is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * GEGL-GTK is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL-GTK; if not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2003, 2004, 2006 Øyvind Kolås
+ * Copyright (C) 2003, 2004, 2006 Øyvind Kolås <pippin@gimp.org>
  */
 
 #ifndef __GEGL_GTK_VIEW_H__
--- operations/gtk-display.c
+++ operations/gtk-display.c
@@ -1,19 +1,19 @@
-/* This file is an image processing operation for GEGL
+/* This file is part of GEGL-GTK
  *
- * GEGL is free software; you can redistribute it and/or
+ * GEGL-GTK is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  *
- * GEGL is distributed in the hope that it will be useful,
+ * GEGL-GTK is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with GEGL; if not, see <http://www.gnu.org/licenses/>.
+ * License along with GEGL-GTK; if not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ * Copyright (C) 2006 Øyvind Kolås <pippin@gimp.org>
  */
 
 #include "config.h"
