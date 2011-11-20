6990a3971d7b988dd13c421e946715409d25562b
Jon Nordby <jononor@gmail.com>
2011-11-20 21:25:22 +0100

    Update/add some URLs

--- gegl-gtk/gegl-gtk.h
+++ gegl-gtk/gegl-gtk.h
@@ -9,11 +9,20 @@
  * For building GEGL-GTK, please refer to the README file included
  * in the source code tarball.
  *
- * GEGL-GTK supports both GTK+ 2 and 3, and
- * can be used directly as a C library, or in languages
- * with bindings provided by GObject Introspection.
+ * GEGL-GTK supports both GTK+ 2 and 3. It can be used directly as a C library,
+ * or in languages with bindings provided by GObject Introspection.
  *
- * For usage examples, see http://git.gnome.org/browse/gegl-gtk/tree/examples
+ * GEGL-GTK is licensed under the GNU LGPL v3+, and maintained by Jon Nordby
+ *
+ * Homepage: <ulink url="http://www.gegl.org/gegl-gtk">http://www.gegl.org/gegl-gtk</ulink>
+ *
+ * Code: <ulink url="http://git.gnome.org/browse/gegl-gtk/">http://git.gnome.org/browse/gegl-gtk/</ulink>
+ *
+ * Bugtracker: <ulink url="http://bugs.gnome.org">http://bugs.gnome.org</ulink>, product GEGL, component gegl-gtk
+ *
+ * Mailinglist: <ulink url="http://mail.gnome.org/mailman/listinfo/gegl-developer-list">gegl-developer@lists.gnome.org</ulink>
+ *
+ * For usage examples, see <ulink url="http://git.gnome.org/browse/gegl-gtk/tree/examples">http://git.gnome.org/browse/gegl-gtk/tree/examples</ulink>
  *
  * GEGL-GTK provides one class: #GeglGtkView
  **/
